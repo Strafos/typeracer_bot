db45f068f44dc62025c00b2e6e2b42ee2a0e83d2
Zaibo Wang <zaibowang@gmail.com>
2017-07-13 16:38:21 -0400

    Add anti-anti-cheating measures

--- typer.py
+++ typer.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
+import nltk
 
 def find(driver):
     elems = driver.find_elements_by_xpath('//*[@id="gwt-uid-15"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span')
@@ -23,18 +25,45 @@ while 'final' not in elem.text:
 print 'COUNTDOWN'
 time.sleep(5)
 print 'time to start'
-time.sleep(3)
 
 # elems = driver.find_elements_by_xpath('//*[@id="gwt-uid-15"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span')
 # print elem.get_attribute('text')
 elems = WebDriverWait(driver, 1).until(find)
-actions = ActionChains(driver)
+text = ''
 for elem in elems:
-    print elem.text
     if len(elem.text) > 10:
-        actions.send_keys(' ')
-    actions.send_keys(elem.text)
-actions.perform()
+        text += ' '
+    text += elem.text
+print text
 
-time.sleep(10)
+# driver.find_element_by_xpath('//*[@id="tstats-edit"]/table/tbody/tr/td[1]/a').click()
+# login = ActionChains(driver)
+# login.send_keys('\' + Keys.ENTER)
+# login.perform()
+
+### ActionChains approach
+# actions = ActionChains(driver)
+# for elem in elems:
+#     print elem.text
+#     if len(elem.text) > 10:
+#         actions.send_keys(' ')
+#     actions.send_keys(elem.text)
+# actions.perform()
+
+text_input = driver.find_element_by_xpath('//*[@id="gwt-uid-15"]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/input')
+while text.find(' ') != -1:
+    idx = text.index(' ')
+    text_input.send_keys(text[:idx] + ' ')
+    text = text[idx+1:]
+    time.sleep(.4)
+text_input.send_keys(text)
+    
+PUNCTUATION = [',', ':', '.', '(', ')', '!', '"', "'"]
+# for token in tokens:
+#     if token in PUNCTUATION:
+#         text_input.send_keys(Keys.BACKSPACE)
+#     text_input.send_keys(token + ' ')
+#     time.sleep(.2)
+
+time.sleep(1000)
 driver.quit()
